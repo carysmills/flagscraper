a058514da0a586985a4ec403e3af1b437c762060
Carys Mills <mills.carys@gmail.com>
2017-08-17 07:03:07 -0400

    Update scraper2.py

--- scraper2.py
+++ scraper2.py
@@ -2,13 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import datefinder
-import re
+import re #for regular expressions
 import dateutil
 from datetime import datetime
-import sys
-import tweepy, time, sys
-import lxml
+import sys #for UTF and twitter
+import tweepy, time 
+import lxml #to use with BS
 
+#for Twitter
 CONSUMER_KEY = 'tClwWO7mTLmmXLoPvPa0WMbVt'
 CONSUMER_SECRET = 'PbWfOVnWLua0Nv52SHBiV7aL7BDaa0jiw4RWbMfG1dVhdJfzko'
 ACCESS_KEY = '858776811731394560-mJReLVQPvyvFYu6Zkmwn4BuYyIVzizH'
@@ -20,13 +21,14 @@ api = tweepy.API(auth)
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+#go look at the RSS feed for half-masting notices
 r  = requests.get("http://canada.pch.gc.ca/eng/rss?searchFilter=008")
 data = r.text
 soup = BeautifulSoup(data, "lxml")
 containers = soup.findAll("item")
 index1 = 0
 
-# check the old file to see the most recent pubdate
+# check the existing CSV to see the most recent pubdate
 with open('/var/www/flagscraper/flagscraper/static/flagdata2.csv', 'r') as oldfile:
 	oldcsvcontent = csv.DictReader(oldfile, delimiter='|')
 	for row in oldcsvcontent:
@@ -39,17 +41,19 @@ with open('/var/www/flagscraper/flagscraper/static/flagdata2.csv', 'a') as csvfi
 		latestdate = containers[index1].find("pubdate").text.encode('utf-8').strip()
 		break
 
+# 	compare the new date and old date
 	if (latestdate == oldpubdate):
 		print "No new records."
 
 	else:
+		#go through the many stages to save the correst data to the CSV
 		for i in reversed(containers):
 			title = containers[index1].find("title").text.encode('utf-8').strip()
 			pubdate = containers[index1].find("pubdate").text.encode('utf-8').strip()
 			info = containers[index1].find("description").text.encode('utf-8').strip()
 			newpubdate = dateutil.parser.parse(pubdate)
 
-			# isolate masting period
+			# isolate masting period using known language
 			start = 'Period:'
 			end = 'Occasion'
 			s = info
@@ -88,14 +92,14 @@ with open('/var/www/flagscraper/flagscraper/static/flagdata2.csv', 'a') as csvfi
 			end2 = 'Additional'
 			location = s[s.find(start2)+len(start2):s.rfind(end2)]
 
-			# how to get the section number by itself
+			# get the section number by itself
 			section = containers[index1].findAll("a")
 
-			# get the section links
+			# get the section links to determine number of sections cited
 			for link in section:
 				linktitle = link.text
 
-			# if more than one section or and, make variable be called multiple. otherwise just get the section by itself
+			# if more than one section or 'and', make variable be called multiple. otherwise just get the section by itself
 			if (len(section) > 1) or ("and" in linktitle) or ("Sections" in linktitle) or ("And" in linktitle) or ("&" in linktitle):
 				sectionno = "Multiple"
 			else:
@@ -106,7 +110,7 @@ with open('/var/www/flagscraper/flagscraper/static/flagdata2.csv', 'a') as csvfi
 			# write to the CSV
 			csvwriter.writerow([title, pubdate, mastingperiod, occasion, location, sectionno, date1, date2, year])
 
-			# tweet bots
+			# tweet bot, created a tweet and send
 			func = lambda s: s[:1].lower() + s[1:] if s else ''
 
 			tweet = "Flags will be at half mast for: " + title 
@@ -114,7 +118,7 @@ with open('/var/www/flagscraper/flagscraper/static/flagdata2.csv', 'a') as csvfi
 			tweetfinal = tweet[:113] + "... " + link2
 			api.update_status(status=tweetfinal)
 
-			# iterate
+			# iterate -- make sure this only happens once
 			index1+=1
 			break
 
